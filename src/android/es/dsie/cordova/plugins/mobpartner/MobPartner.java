3da8fd00b914ed20d9e02c6173acb42eb064602c
labs@dsi-eritia.es <>
2014-07-11 06:57:05 +0000

    Changes

---
+++ src/android/es/dsie/cordova/plugins/mobpartner/MobPartner.java
@@ -0,0 +1,65 @@
+package com.google.cordova.plugin;
+
+import java.util.Iterator;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.LinearLayoutSoftKeyboardDetect;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.mobpartner.android.publisher.views.MobPartnerAdBanner;
+
+import android.util.Log;
+
+public class MobPartnerPlugin extends CordovaPlugin {
+    private static final String ACTION_INIT = "init";
+    private static final String ACTION_SHOW_BANNER = "showBanner";
+    private static final String ACTION_SHOW_INTERSTITIAL = "showInterstitial";
+    private static final String ACTION_HIDE_BANNER = "hideBanner";
+
+    @Override
+    public boolean execute(String action, JSONArray inputs, CallbackContext callbackContext) throws JSONException {
+        if (ACTION_INIT.equals(action)) {
+            //executeCreateBannerView(inputs, callbackContext);
+            executeInit(inputs,callbackContext);
+            return true;
+        } else if (ACTION_SHOW_BANNER.equals(action)) {
+            //executeCreateInterstitialView(inputs, callbackContext);
+            executeShowBanner(inputs,callbackContext);
+            return true;
+        } else if (ACTION_SHOW_INTERSTITIAL.equals(action)) {
+            //executeRequestAd(inputs, callbackContext);
+            return true;
+        } else if (ACTION_HIDE_BANNER.equals(action)) {
+            //executeKillAd(callbackContext);
+            return true;
+        } else {
+            Log.d(LOGTAG, String.format("Invalid action passed: %s", action));
+            callbackContext.error("Invalid Action");
+        }
+        return false;
+    }
+
+    private void executeShowBanner(JSONArray inputs, CallbackContext callbackContext) {
+        String poolId = "";
+        String size = "";
+
+        // Get the input data.
+        try {
+            poolId = inputs.getString(0);
+            
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(FrameLayout.LayoutParams.FILL_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+            banner = new MobPartnerAdBanner(this, poolId);
+            banner.setLayoutParams(layoutParams); 
+            banner.show();
+
+        } catch (JSONException exception) {
+            Log.w(LOGTAG,String.format("Got JSON Exception: %s",exception.getMessage()));
+            callbackContext.error(exception.getMessage());
+        }
+    }
+
+}
